30d1b97251d4de375287da70b8849a0909d28dc8
System Administrator <root@macbook.local>
2024-01-14 21:30:32 +0500

    добавил 3 страницы

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/Ego', function(req, res, next) { res.send("<h1>Страница Ego</h1>")
+});
+router.get('/Selflove', function(req, res, next) { res.send("<h1>Страница Selflove</h1>")
+});
+router.get('/selfrespect', function(req, res, next) { res.send("<h1>Страница self respect</h1>")
+});
 module.exports = router;
